b0c0fcffe62ce0089090da7d952611b3a745cf6d
Patryk-Stefanski <patrykstefanski00@gmail.com>
2021-10-13 14:24:42 +0100

    Refactor : Template list page: HTTP API call centralized.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,28 +1,29 @@
-import React, {useState, useEffect}  from "react";
+import React, { useState, useEffect } from "react";
 import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 const images = [
     "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
     "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
     "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
     "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-  ]
+]
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridList: {
-    width: 450,
-    height: "100vh",
-  },
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+    },
+    gridList: {
+        width: 450,
+        height: "100vh",
+    },
 }));
 
 const MoviePage = (props) => {
@@ -31,69 +32,56 @@ const MoviePage = (props) => {
     const [movie, setMovie] = useState(null);
     const [images, setImages] = useState([]);
     useEffect(() => {
-        fetch(
-          `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .then((movie) => {
-            // console.log(movie)
+        getMovie(id).then((movie) => {
             setMovie(movie);
-          });
-      }, [id]);
-    
-      useEffect(() => {
-        fetch(
-          `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-        )
-          .then((res) => res.json())
-          .then((json) => json.posters)
-          .then((images) => {
-            // console,log(images)
-            setImages(images);
-          });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+        });
+    }, [id]);
 
-  return (
-    <>
-      {movie ? (
+    useEffect(() => {
+        getMovieImages(id).then((images) => {
+            setImages(images);
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
         <>
-          <MovieHeader movie={movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div className={classes.root}>
-                <GridList
-                  cellHeight={500}
-                  className={classes.gridList}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <GridListTile
-                      key={image.file_path}
-                      className={classes.gridListTile}
-                      cols={1}
-                    >
-                    <img
-                      src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                      alt={image.file_path}
-                    />
-                    </GridListTile>
-                  ))}
-                </GridList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <MovieDetails movie={movie} />
-            </Grid>
-          </Grid>
+            {movie ? (
+                <>
+                    <MovieHeader movie={movie} />
+                    <Grid container spacing={5} style={{ padding: "15px" }}>
+                        <Grid item xs={3}>
+                            <div className={classes.root}>
+                                <GridList
+                                    cellHeight={500}
+                                    className={classes.gridList}
+                                    cols={1}
+                                >
+                                    {images.map((image) => (
+                                        <GridListTile
+                                            key={image.file_path}
+                                            className={classes.gridListTile}
+                                            cols={1}
+                                        >
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                                                alt={image.file_path}
+                                            />
+                                        </GridListTile>
+                                    ))}
+                                </GridList>
+                            </div>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <MovieDetails movie={movie} />
+                        </Grid>
+                    </Grid>
+                </>
+            ) : (
+                <h2>Waiting for API data</h2>
+            )}
         </>
-      ) : (
-        <h2>Waiting for API data</h2>
-      )}
-    </>
-  );
+    );
 };
 
 export default MoviePage
