020198dac833f927c5ca3b76b8619a44df5e347d
Patryk-Stefanski <patrykstefanski00@gmail.com>
2021-10-19 14:16:24 +0100

    Show movie review excerpts; Refactor Details page.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,87 +1,32 @@
 import React, { useState, useEffect } from "react";
-import MovieHeader from "../components/headerMovie/";
-import MovieDetails from "../components/movieDetails/";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
+import { withRouter } from "react-router-dom";
+import MovieDetails from "../components/movieDetails";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
-const images = [
-    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-]
+const MovieDetailsPage = (props) => {
+  const { id } = props.match.params;
+  const [movie, setMovie] = useState(null);
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-    },
-    gridList: {
-        width: 450,
-        height: "100vh",
-    },
-}));
+  useEffect(() => {
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
+  }, [id]);
 
-const MoviePage = (props) => {
-    const classes = useStyles();
-    const { id } = props.match.params;
-    const [movie, setMovie] = useState(null);
-    const [images, setImages] = useState([]);
-    useEffect(() => {
-        getMovie(id).then((movie) => {
-            setMovie(movie);
-        });
-    }, [id]);
-
-    useEffect(() => {
-        getMovieImages(id).then((images) => {
-            setImages(images);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
+  return (
+    <>
+      {movie ? (
         <>
-            {movie ? (
-                <>
-                    <MovieHeader movie={movie} />
-                    <Grid container spacing={5} style={{ padding: "15px" }}>
-                        <Grid item xs={3}>
-                            <div className={classes.root}>
-                                <GridList
-                                    cellHeight={500}
-                                    className={classes.gridList}
-                                    cols={1}
-                                >
-                                    {images.map((image) => (
-                                        <GridListTile
-                                            key={image.file_path}
-                                            className={classes.gridListTile}
-                                            cols={1}
-                                        >
-                                            <img
-                                                src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                                                alt={image.file_path}
-                                            />
-                                        </GridListTile>
-                                    ))}
-                                </GridList>
-                            </div>
-                        </Grid>
-                        <Grid item xs={9}>
-                            <MovieDetails movie={movie} />
-                        </Grid>
-                    </Grid>
-                </>
-            ) : (
-                <h2>Waiting for API data</h2>
-            )}
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
         </>
-    );
+      ) : (
+        <p>Waiting for movie details</p>
+      )}
+    </>
+  );
 };
 
-export default MoviePage
+export default withRouter(MovieDetailsPage);
